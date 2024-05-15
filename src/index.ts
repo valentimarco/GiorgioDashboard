167f5d68608a6d8fa21779077b63717f7f0bf53c
valentimarco <gamespy67@gmail.com>
2024-05-15 11:35:41 +0200

    Add: Bun bundler

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import $ from "jquery"
+import jQuery from "jquery"
+import htmx from "htmx.org"
+import { updateTitle } from "./utils"
+
+window.htmx = htmx
+window.$ = $
+window.jQuery = jQuery
+window.updateTitle = updateTitle
---
+++ src/utils.ts
@@ -0,0 +1,3 @@
+export function updateTitle(element: HTMLButtonElement) {
+    $('#titlemainbar').text($(element).text());
+}
